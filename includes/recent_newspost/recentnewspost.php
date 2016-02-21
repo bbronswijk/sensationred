e2e7cf82c12b10ff2208c0e6998abf96e5ad658e
bbronswijk <brambronswijk@gmail.com>
2016-02-21 11:54:25 +0100

    nieuwsberichten in de newsposts widget heeft nu id, iframe is responsive

--- includes/recent_newspost/recentnewspost.php
+++ includes/recent_newspost/recentnewspost.php
@@ -21,64 +21,41 @@ wp_enqueue_style( 'style_recentposts', get_template_directory_uri().'/includes/r
 	
 
 	public function widget( $args, $instance ) {
-		 
-				
-				
 				
 				$recent_posts = new WP_Query('showposts=2');	
 						if ( $recent_posts -> have_posts() ) {
 							
 							echo "<div class='recentpost_widget'>";
-							echo "<h1>" . __( 'Latest news','sensationred') . "</h1>";
+								echo "<h1>" . __( 'Latest news','sensationred') . "</h1>";
 							
 								while ( $recent_posts -> have_posts() ) {
 									$recent_posts -> the_post(); 
-									?>
-									
-										<article class="RPwidget_recent_post"><?php
-											
-											
-											global $more;
-											$more = 0;
-											?>
-											
-												<?php
-												if ( has_post_thumbnail() ) {
-													
-													echo"<div class='post_thumb'><a href="; the_permalink(); echo">";
-										
-													the_post_thumbnail();
-													echo"</a></div> <!-- post thumb -->	";
-													
-												} 
-												?>
-																					
+									?>								
+										<article class="RPwidget_recent_post RPid-<?php echo $recent_posts->post->ID ?>">											
+												<?php if ( has_post_thumbnail() ) { ?>													
+													<div class='post_thumb'>
+														<a href="<?php the_permalink(); ?>">";										
+														<?php the_post_thumbnail(); ?>
+														</a>
+													</div> 
+												<?php }	?>									
 											<div class="post_content"> 
 												<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-												<?php 
-												the_excerpt();
-												
-												?>
-												<a href="<?php the_permalink() ?>" class="readmore"><?php _e( 'Read more','sensationred') ?></a>
-												
-											</div> <!-- post content -->
-											
-										</article> <!-- recent_post -->
-									
-									
-									 <?php
-									//
-								
-								} // end while
-								echo"</div>";
-			
-							} // end if
-		
+												<?php the_excerpt(); ?>
+												<a href="<?php the_permalink() ?>" class="readmore">
+													<?php _e( 'Read more','sensationred') ?>
+												</a>												
+											</div> <!-- /post content -->											
+										</article> <!-- /recent_post -->
+									 <?php								
+								} // end while ?>
+								</div> <!-- /recentpost_widget -->
+							<?php } // end if
 	 }
 
  	public function form( $instance ) {
 		// outputs the options form on admin
-		 echo "Widget activated! The last two newsposts are visible in the sidebar of your website.";
+		 echo "Widget activated! The last two newsposts are now visible in the sidebar of your website.";
 	}
 
 
